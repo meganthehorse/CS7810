7c2af6cba63c53de2d01c2627771c9c617c223f2
bddave <dave.15@wright.edu>
2023-04-03 06:41:18 -0400

    Base for now, Naming Convention TBD

--- code/Triplification_For_Asterank.py
+++ code/Triplification_For_Asterank.py
@@ -1,3 +1,12 @@
+#####
+#
+# This triplification for Asterank_Dataset.csv
+# skips MOID Classification as a pattern has not
+# been described for Classifiers outside the AsteroidClassification
+#
+#####
+
+
 ##### Graph stuff
 import os
 import pandas as pd
@@ -32,117 +41,143 @@ pfs = {
 "time": TIME,
 "qudt": Namespace("http://qudt.org/schema/qudt/"),
 }
-activities = ["Name", "AsteroidClassification", "Eccentricity",
+# Type,a (AU),e,
+# Value ($),Est. Profit ($),Δv (km/s),MOID (AU),Group
+CV_QK = ["AsteroidClassification", "Semi-MajorAxis", "Eccentricity",
               "Value","Profit", "Velocity", "MOID", "MOIDClassification"]
-activity_description = ["Name", "Asteroid Classification", "Eccentricity",
+CV_UNITS = ["","au","au",
+            "Currency","Currency","kms","au","group"]
+ACTIVITIES = ["AsteroidClassification", "Semi-MajorAxis", "Eccentricity",
+              "Value","Profit", "Velocity", "MOID", "MOIDClassification"]
+ACTIVITY_DESC = ["Asteroid Classification", "Semi-Major Axis","Eccentricity",
                         "Value", "Profit", "Velocity", "MOID", "MOID Classification"]
-observable_property = ["PhysicalProperty", "OrbitalProperty", "EconomicProperty"]
+OBSERVABLE_PROPERTIES = ["PhysicalProperty", "OrbitalProperty", "EconomicProperty"]
 
-def triple_value(graph:Graph):
-    ###  Adding Value[6] to SOL Ontology
-    value_observation_uri = pfs["solr"][f"ValueObservation.{discovery}"]
-    value_observable_property_uri = pfs["solr"][f"EconomicProperty.Value.{discovery}"]
-    graph.add( (value_observation_uri, pfs["sol-ont"][f"hasFeatureOfInterest"], asteroid_uri) )
-    graph.add( (value_observation_uri, pfs["sol-ont"]["hasObservableProperty"], value_observable_property_uri) )
-    graph.add( (value_observable_property_uri, a, pfs["sol-ont"]["ObservableProperty"]) )
+def triple_orbital(graph:Graph, data, index):
+    activity = ACTIVITIES[index]
+    description = ACTIVITY_DESC[index]
+    qk = CV_QK[index]
+    unit = CV_UNITS[index]
+    observation_uri = pfs["solr"][f"Observation.{activity}.{discovery}"]
+    observable_property_uri = pfs["solr"][f"OrbitalProperty.{activity}.{discovery}"]
+    graph.add( (observation_uri, pfs["sol-ont"][f"hasFeatureOfInterest"], asteroid_uri) )
+    graph.add( (observation_uri, pfs["sol-ont"]["hasObservableProperty"], observable_property_uri) )
+    graph.add( (observable_property_uri, a, pfs["sol-ont"]["ObservableProperty"]) )
 
-    value_activity_uri = pfs["solr"][f"ValueActivity.{discovery}"]
-    graph.add( (value_activity_uri, pfs["sol-ont"]["hasDescription"], Literal(f"{activity_description[3]}", datatype=XSD.string)) )
-    graph.add( (value_activity_uri, pfs["sol-ont"]["performedBy"], agent_uri) )
-
-    #  Mint the Result and Quantity Nodes
-    value_result_uri = pfs["solr"][f"Result.Value.{discovery}"]
-    value_quantity_uri = pfs["solr"][f"Quantity.Value.{discovery}"]
-    graph.add( (value_quantity_uri, a, pfs["sol-ont"]["Quantity"]) )
+    activity_uri = pfs["solr"][f"Activity.{activity}.{discovery}"]
+    graph.add( (activity_uri, pfs["sol-ont"]["hasDescription"], Literal(f"{description}", datatype=XSD.string)) )
+    graph.add( (activity_uri, pfs["sol-ont"]["performedBy"], agent_uri) )
     
-    value_qk_uri = pfs["solr"][f"QuantityKind.Value.{discovery}"]
-    graph.add( (value_qk_uri, a, pfs["sol-ont"]["QuantityKind"]) )
+    #  Mint the Result and Quantity Nodes
+    result_uri = pfs["solr"][f"Result.{activity}.{discovery}"]
+    graph.add( (result_uri, a, pfs["sol-ont"]["Result"]) )
+    quantity_uri = pfs["solr"][f"Quantity.{activity}.{discovery}"]
+    graph.add( (quantity_uri, a, pfs["sol-ont"]["Quantity"]) )
+        
+    qk_uri = pfs["solr"][f"QuantityKind.{activity}.{discovery}"]
+    graph.add( (qk_uri, a, pfs["sol-ont"]["QuantityKind"]) )
 
-    value_qv_uri = pfs["solr"][f"QuantityValue.Value.{discovery}"]
-    graph.add( (value_qv_uri, a, pfs["sol-ont"]["QuantityValue"]) )
+    qv_uri = pfs["solr"][f"QuantityValue.{activity}.{discovery}"]
+    graph.add( (qv_uri, a, pfs["sol-ont"]["QuantityValue"]) )
 
-    unit_uri = pfs["solr"][f"Unit.Value.{discovery}"]    
+    unit_uri = pfs["solr"][f"Unit.{activity}.{discovery}"]    
     graph.add( (unit_uri, a, pfs["sol-ont"]["Unit"]) )
     
-    value_uri = pfs["solr"][f"NumericValue.Value.{discovery}"]
+    value_uri = pfs["solr"][f"NumericValue.{activity}.{discovery}"]
 
     #  Declare the Result schema into the SOL Ontology
-    graph.add( (value_result_uri, pfs["sol-ont"]["hasQuantity"], value_quantity_uri) )
-    graph.add( (value_quantity_uri, pfs["sol-ont"]["hasQuantityKind"], value_qk_uri) ) 
-    graph.add( (value_qk_uri, a, Literal("Currency", datatype=pfs["sol-qk"]["Currency"])) )
+    graph.add( (result_uri, pfs["sol-ont"]["hasQuantity"], quantity_uri) )
+    graph.add( (quantity_uri, pfs["sol-ont"]["hasQuantityKind"], qk_uri) ) 
+    graph.add( (qk_uri, a, Literal(qk, datatype=pfs["sol-qk"][qk])) )
 
-    graph.add( (value_quantity_uri, pfs["sol-ont"]["hasQuantityValue"], value_uri) )
+    graph.add( (quantity_uri, pfs["sol-ont"]["hasQuantityValue"], value_uri) )
+    graph.add( (value_uri, pfs["sol-ont"]["hasUnit"], unit_uri) )
+
+    graph.add( (unit_uri, a, Literal(unit, datatype=pfs["sol-unit"][unit])) )
+    graph.add( (value_uri, pfs["sol-ont"]["hasNumericValue"], Literal(data, datatype=XSD.double)) ) 
+
+    #  Connect Observation to EWP Relationships
+    graph.add( (observation_uri, pfs["sol-ont"]["attributedTo"], agent_uri) )
+    graph.add( (observation_uri, pfs["sol-ont"]["generatedBy"], activity_uri) )
+
+    #  Connect Result to Observation        
+    graph.add( (observation_uri, pfs["sol-ont"]["hasResult"], result_uri) )
+
+
+def triple_economic(graph:Graph, numeric, index):
+    activity = ACTIVITIES[index]
+    observation_uri = pfs["solr"][f"{activity}Observation.{discovery}"]
+    observable_property_uri = pfs["solr"][f"EconomicProperty.{activity}.{discovery}"]
+    graph.add( (observation_uri, pfs["sol-ont"][f"hasFeatureOfInterest"], asteroid_uri) )
+    graph.add( (observation_uri, pfs["sol-ont"]["hasObservableProperty"], observable_property_uri) )
+    graph.add( (observable_property_uri, a, pfs["sol-ont"]["ObservableProperty"]) )
+
+    activity_uri = pfs["solr"][f"Activity.{activity}.{discovery}"]
+    graph.add( (activity_uri, pfs["sol-ont"]["hasDescription"], Literal(f"{ACTIVITY_DESC[index]}", datatype=XSD.string)) )
+    graph.add( (activity_uri, pfs["sol-ont"]["performedBy"], agent_uri) )
+    
+    #  Mint the Result and Quantity Nodes
+    result_uri = pfs["solr"][f"Result.{activity}.{discovery}"]
+    graph.add( (result_uri, a, pfs["sol-ont"]["Result"]) )
+    quantity_uri = pfs["solr"][f"Quantity.{activity}.{discovery}"]
+    graph.add( (quantity_uri, a, pfs["sol-ont"]["Quantity"]) )
+        
+    qk_uri = pfs["solr"][f"QuantityKind.{activity}.{discovery}"]
+    graph.add( (qk_uri, a, pfs["sol-ont"]["QuantityKind"]) )
+
+    qv_uri = pfs["solr"][f"QuantityValue.{activity}.{discovery}"]
+    graph.add( (qv_uri, a, pfs["sol-ont"]["QuantityValue"]) )
+
+    unit_uri = pfs["solr"][f"Unit.{activity}.{discovery}"]    
+    graph.add( (unit_uri, a, pfs["sol-ont"]["Unit"]) )
+    
+    value_uri = pfs["solr"][f"NumericValue.{activity}.{discovery}"]
+
+    #  Declare the Result schema into the SOL Ontology
+    graph.add( (result_uri, pfs["sol-ont"]["hasQuantity"], quantity_uri) )
+    graph.add( (quantity_uri, pfs["sol-ont"]["hasQuantityKind"], qk_uri) ) 
+    graph.add( (qk_uri, a, Literal("Currency", datatype=pfs["sol-qk"]["Currency"])) )
+
+    graph.add( (quantity_uri, pfs["sol-ont"]["hasQuantityValue"], value_uri) )
     graph.add( (value_uri, pfs["sol-ont"]["hasUnit"], unit_uri) )
 
     graph.add( (unit_uri, a, Literal("USD", datatype=pfs["sol-unit"]["USD"])) )
-    value_split = tokens[6].split(" ")
-    if(value_split[1] == "million"):
-        value=float(value_split[0])*math.pow(10, 6)
-    elif(value_split[1] == "billion"):
-        value=float(value_split[0])*math.pow(10, 9)
-    elif(value_split[1] == "trillion"):
-        value=float(value_split[0])*math.pow(10, 12)
+    split = numeric.split(" ")
+    if(split[1] == "million"):
+        value=float(split[0])*math.pow(10, 6)
+    elif(split[1] == "billion"):
+        value=float(split[0])*math.pow(10, 9)
+    elif(split[1] == "trillion"):
+        value=float(split[0])*math.pow(10, 12)
     graph.add( (value_uri, pfs["sol-ont"]["hasNumericValue"], Literal(value, datatype=XSD.double)) ) 
 
     #  Connect Observation to EWP Relationships
-    graph.add( (value_observation_uri, pfs["sol-ont"]["attributedTo"], agent_uri) )
-    graph.add( (value_observation_uri, pfs["sol-ont"]["generatedBy"], value_activity_uri) )
+    graph.add( (observation_uri, pfs["sol-ont"]["attributedTo"], agent_uri) )
+    graph.add( (observation_uri, pfs["sol-ont"]["generatedBy"], activity_uri) )
 
     #  Connect Result to Observation        
-    graph.add( (value_observation_uri, pfs["sol-ont"]["hasResult"], value_result_uri) )
+    graph.add( (observation_uri, pfs["sol-ont"]["hasResult"], result_uri) )
 
-def triple_profit(graph:Graph):
-    profit_observation_uri = pfs["solr"][f"ProfitObservation.{discovery}"]
-    profit_observable_property_uri = pfs["solr"][f"EconomicProperty.Profit.{discovery}"]
-    graph.add( (profit_observation_uri, pfs["sol-ont"][f"hasFeatureOfInterest"], asteroid_uri) )
-    graph.add( (profit_observation_uri, pfs["sol-ont"]["hasObservableProperty"], profit_observable_property_uri) )
-    graph.add( (profit_observable_property_uri, a, pfs["sol-ont"]["ObservableProperty"]) )
+def triple_type(graph:Graph, type, index=1):
+    activity = ACTIVITIES[index]
+    #  Mint
+    classification_uri = pfs["solr"][f"{activity}.{discovery}"]
+    graph.add( (classification_uri, a, pfs["sol-ont"]["AsteroidClassification"]) )
+    smassii_uri = pfs["solr"][f"SMASSII.{discovery}"]
+    graph.add( (smassii_uri, a, pfs["sol-ont"]["SMASSIIClass"]) )
+    graph.add( (asteroid_uri, pfs["sol-ont"][f"hasAsteroidClassification"], classification_uri) )
 
-    profit_activity_uri = pfs["solr"][f"ProfitActivity.{discovery}"]
-    graph.add( (profit_activity_uri, pfs["sol-ont"]["hasDescription"], Literal(f"{activity_description[4]}", datatype=XSD.string)) )
-    graph.add( (profit_activity_uri, pfs["sol-ont"]["performedBy"], agent_uri) )
+    #  Declare
+    graph.add( (classification_uri, pfs["sol-ont"]["hasSMASSIIClass"], smassii_uri) )
+    graph.add( (smassii_uri, pfs["sol-ont"]["hasLabel"], Literal(type, datatype=RDFS.label)) )
+    ##  TODO:  Confirm ElementalComposition link from Triplification of Taxonomy
+    activity_uri = pfs["solr"][f"Activity.{activity}.{discovery}"]
+    graph.add( (activity_uri, pfs["sol-ont"]["hasDescription"], Literal(f"{ACTIVITY_DESC[index]}", datatype=XSD.string)) )
+    graph.add( (activity_uri, pfs["sol-ont"]["performedBy"], agent_uri) )
 
-    #  Mint the Result and Quantity Nodes
-    profit_result_uri = pfs["solr"][f"Result.Profit.{discovery}"]
-    profit_quantity_uri = pfs["solr"][f"Quantity.Profit.{discovery}"]
-    graph.add( (profit_quantity_uri, a, pfs["sol-ont"]["Quantity"]) )
-    
-    profit_qk_uri = pfs["solr"][f"QuantityKind.Profit.{discovery}"]
-    graph.add( (profit_qk_uri, a, pfs["sol-ont"]["QuantityKind"]) )
-
-    profit_qv_uri = pfs["solr"][f"QuantityValue.Profit.{discovery}"]
-    graph.add( (profit_qv_uri, a, pfs["sol-ont"]["QuantityValue"]) )
-
-    unit_uri = pfs["solr"][f"Unit.Profit.{discovery}"]    
-    graph.add( (unit_uri, a, pfs["sol-ont"]["Unit"]) )
-    
-    value_uri = pfs["solr"][f"NumericValue.Profit.{discovery}"]
-
-    #  Declare the Result schema into the SOL Ontology
-    graph.add( (profit_result_uri, pfs["sol-ont"]["hasQuantity"], profit_quantity_uri) )
-    graph.add( (profit_quantity_uri, pfs["sol-ont"]["hasQuantityKind"], profit_qk_uri) ) 
-    graph.add( (profit_qk_uri, a, Literal("Currency", datatype=pfs["sol-qk"]["Currency"])) )
-
-    graph.add( (profit_quantity_uri, pfs["sol-ont"]["hasQuantityValue"], value_uri) )
-    graph.add( (value_uri, pfs["sol-ont"]["hasUnit"], unit_uri) )
-
-    graph.add( (unit_uri, a, Literal("USD", datatype=pfs["sol-unit"]["USD"])) )
-    profit_split = tokens[7].split(" ")
-    if(profit_split[1] == "million"):
-        profit=float(profit_split[0])*math.pow(10, 6)
-    elif(profit_split[1] == "billion"):
-        profit=float(profit_split[0])*math.pow(10, 9)
-    elif(profit_split[1] == "trillion"):
-        profit=float(profit_split[0])*math.pow(10, 12)
-    graph.add( (value_uri, pfs["sol-ont"]["hasNumericValue"], Literal(profit, datatype=XSD.double)) ) 
-
-
-    #  Connect Observation to EWP Relationships
-    graph.add( (profit_observation_uri, pfs["sol-ont"]["attributedTo"], agent_uri) )
-    graph.add( (profit_observation_uri, pfs["sol-ont"]["generatedBy"], profit_activity_uri) )
-
-    #  Connect Result to Observation        
-    graph.add( (profit_observation_uri, pfs["sol-ont"]["hasResult"], profit_result_uri) )
+    #  Connect AC to EWP Relationships
+    graph.add( (classification_uri, pfs["sol-ont"]["attributedTo"], agent_uri) )
+    graph.add( (classification_uri, pfs["sol-ont"]["generatedBy"], activity_uri) )
 
 # Initialization shortcut
 def init_kg(prefixes=pfs):
@@ -170,6 +205,7 @@ graph.add( (agent_uri, pfs["sol-ont"]["hasName"], Literal("SkyLive", datatype=XS
 
 # for line in lines[1:]:  #  for each asteroid
 for line in lines[1:2]:  
+    # Discovery Name,Common Name,Asterank Name,Type,a (AU),e,Value ($),Est. Profit ($),Δv (km/s),MOID (AU),Group
     tokens = line.split(",")
     discovery = tokens[0]
     split = line.split(",")
@@ -186,13 +222,22 @@ for line in lines[1:2]:
 
     #  Adding Orbital Data to SOL Ontology
     ##  TODO:  Add AC_Observation with OrbitalProperty
-    ##  OrbitalProperty: semi-major, eccentricity, velocity, moid, moidclass
-
+    triple_type(graph, tokens[3])
+    ##  OrbitalProperty: semi-major, eccentricity, velocity, moid
+    orbitIndices = [4, 5, 8, 9]
+    for i in orbitIndices:
+        data = tokens[i]
+        triple_orbital(graph, data, i-3)
     #  Adding Economics to SOL Ontology
     ###  Adding Value[3] to SOL Ontology
-    triple_value(graph)
+    economic_data = [tokens[6], tokens[7]]
+    index = 3
+    for data in economic_data:
+        triple_economic(graph, data, index)
+        index+=1
+    # triple_value(graph)
     ###  Adding Profit[4] to SOL Ontology
-    triple_profit(graph)
+    # triple_profit(graph)
     
 output_file = os.path.join(output_path, f"SOL_Asteroid_Asterank.ttl")
 temp = graph.serialize(format="turtle", encoding="utf-8", destination=output_file)
